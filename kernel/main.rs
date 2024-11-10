570ee8bb594b24c3646c4a15bdd6efa77922ca1e
Petr Holčák <petr7698@gmail.com>
2024-11-10 07:46:59 +0100

    Move Multiboot header from assembly to Rust

--- kernel/main.rs
+++ kernel/main.rs
@@ -6,6 +6,21 @@ mod vga;
 use core::arch::global_asm;
 use core::panic::PanicInfo;
 
+#[repr(C)]
+struct Multiboot {
+	magic: u32,
+	flags: u32,
+	checksum: i32,
+}
+
+#[allow(dead_code)]
+#[link_section = ".multiboot"]
+static MULTIBOOT: Multiboot = Multiboot {
+	magic: 0x1badb002,
+	flags: 0x00,
+	checksum: -(0x1badb002 + 0x00),
+};
+
 global_asm!(include_str!("entry.S"));
 
 #[panic_handler]
